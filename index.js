939108166c197ab8bb6fa4c2a55ce4437a529958
chandrasoumya <soumyadeepc108@gmail.com>
2024-03-23 15:22:55 +0530

    added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors")
-const port = 5000;
+const port =process.env.port || 5000;
 const ProjectsRouter = require("./db/router/projects");
 
 app.use(express.json());
